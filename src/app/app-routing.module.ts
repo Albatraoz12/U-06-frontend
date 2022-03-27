a7e90fe07b4e7bec3fa3c1e47242d6bf0eba62b9
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-03-27 12:11:33 +0200

    fix new routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './public/home/home.component';
 import { PublicComponent } from './public/public.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PublicComponent,
+    children: [{ path: '', component: HomeComponent }],
   },
 ];
 
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicComponent } from './public.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [PublicComponent],
-  imports: [CommonModule],
+  declarations: [PublicComponent, HomeComponent],
+  imports: [CommonModule, RouterModule],
 })
 export class PublicModule {}
