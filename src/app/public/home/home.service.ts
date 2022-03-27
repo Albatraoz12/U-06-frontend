5823b5cff168a90dbdb85ef453ee1b279ad53dc4
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-03-27 12:36:57 +0200

    fix home.service.ts

--- src/app/public/home/home.service.ts
+++ src/app/public/home/home.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { Food } from 'src/app/interface/food';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HomeService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  private foodUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  getFood(): Observable<Food[]> {
+    return this.http.get<Food[]>(this.foodUrl).pipe(
+      tap((d) => console.log('api response', JSON.stringify(d))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    console.log(err);
+    return throwError(errorMessage);
+  }
 }
