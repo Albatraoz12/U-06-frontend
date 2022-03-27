1c1fb1ac77b79ecc6a0d037a3521ab4447d017fc
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-03-27 12:59:47 +0200

    load content from api to homescreen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
@@ -8,7 +9,13 @@ import { PublicModule } from './public/public.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PublicModule, RouterModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PublicModule,
+    RouterModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -8,7 +8,38 @@ import { HomeService } from './home.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  pageTitle: string = 'Dimos Recipe App';
+  food: Food[] = [];
+  filteredFood: Food[] = [];
+  private _listFilter: string = '';
+  errMessage: string = '';
 
-  ngOnInit(): void {}
+  constructor(private homeService: HomeService) {}
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredFood = this.performFiltration(value);
+  }
+
+  performFiltration(filterBy: string): Food[] {
+    filterBy = filterBy.toLowerCase();
+    return this.food.filter((user: Food) =>
+      user.name.toLowerCase().includes(filterBy)
+    );
+  }
+
+  ngOnInit(): void {
+    console.log('I Am loading on ngOnInit');
+    this.listFilter = '';
+    this.homeService.getFood().subscribe({
+      next: (food) => {
+        this.food = food;
+        this.filteredFood = this.food;
+      },
+      error: (err) => (this.errMessage = err),
+    });
+  }
 }
